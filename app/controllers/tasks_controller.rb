169b1fdc9de062963d92753b0304044d0ca88819
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-20 15:53:19 -0700

    Created Task model and replaced hardcoded data in index with db data

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,14 +1,13 @@
 class TasksController < ApplicationController
-require "date"
 
-TASKS = [
-  {id: 1, title: "Don't pick up the phone, you know he's only calling cause he's drunk and alone", status: :not_done},
-  {id: 2, title: "Don't let him in, you'll have to kick him out again", status: :not_done},
-  {id: 3, title: "Don't be his friend, you know you're gonna wake up in his bed in the morning", status: :not_done}
-]
+# TASKS = [
+#   {id: 1, title: "Don't pick up the phone, you know he's only calling cause he's drunk and alone", status: :not_done},
+#   {id: 2, title: "Don't let him in, you'll have to kick him out again", status: :not_done},
+#   {id: 3, title: "Don't be his friend, you know you're gonna wake up in his bed in the morning", status: :not_done}
+# ]
 
   def index
-    @tasks = TASKS
+    @tasks = Task.all.order(:id)
     @date = Time.now.to_date
   end
 
