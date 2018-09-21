e29678f9a0fcefcdcac893f518c6bde2dc745b6f
Sammi-Jo Lee <sjlee3157@gmail.com>
2018-09-20 18:50:30 -0700

    Can edit and update tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -15,17 +15,41 @@ class TasksController < ApplicationController
 
   def create
     # other invalid params?
-    if params[:task][:name] == "" || params[:task][:description] == ""
+    name = params[:task][:name]
+    description = params[:task][:description]
+    if name == "" || description == ""
       render:new
     else
-      @task = Task.new(name: params[:task][:name],
-                       description: params[:task][:description])
+      @task = Task.new(name: name,
+                       description: description)
     end
     if @task.save
       redirect_to root_path
     else
       render :new
     end
+  end
+
+# for now, just edit the name, not the description
+# the site should take the user back to the task's show page after the task is updated
+  def edit
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
+      # get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  end
+
+  def update
+    id = params[:id].to_i
+    @task = Task.find_by(id: id)
+    new_name = params[:task][:name]
+    new_decription = params[:task][:description]
+# edge case: nil
+    if @task.update(name: new_name, description: new_decription)
+      redirect_to task_path(id)
+    else
+      render :edit
+    end
+      # patch '/tasks/:id', to: 'tasks#update', as: 'update_task'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,16 @@ Rails.application.routes.draw do
   root 'tasks#index'
 
   get '/tasks/new', to: 'tasks#new', as: 'new_task'
-  post 'tasks', to: 'tasks#create', as: 'tasks'
-  # Why is post /tasks and tasks_path? What happens if I use something else?
+  post '/tasks', to: 'tasks#create'
+  # Why is it post '/tasks'? couldn't it be ANYTHING?
+
+  get '/tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
+  patch '/tasks/:id', to: 'tasks#update'
+  # How does this patch path work?
+  # does the order of stuff in here matter or something?
+  # so confused
+  # does :id come from the params memory or something?
+
   get '/tasks/:id', to: 'tasks#show', as: 'task'
+
 end
